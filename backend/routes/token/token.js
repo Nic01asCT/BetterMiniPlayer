0bb433f1c529694017b214ae6af4be720857001a
Nic01asCT <131681000+Nic01asCT@users.noreply.github.com>
2024-08-13 11:31:21 +0200

    Update handeling in res

--- backend/routes/token/token.js
+++ backend/routes/token/token.js
@@ -5,8 +5,16 @@ const { login, callback, refresh } = require('./tokenService')
 
 router.get('/login', login, (req, res) => { })
 
-router.get('/callback', callback, (req, res) => { })
+router.get('/callback', callback, (req, res) => {
+    const { access_token, refresh_token } = res.token
+    
+    res.json({ access_token, refresh_token })
+})
 
-router.get('/refresh', refresh, (req, res) => { })
+router.get('/refresh', refresh, (req, res) => {
+    const { access_token, refresh_token } = res.token
+
+    res.json({ access_token, refresh_token })
+})
 
 module.exports = router
